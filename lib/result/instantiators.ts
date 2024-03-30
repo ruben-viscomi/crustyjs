ab20d78557409a9bbc61722538df5353c414bb63
Ruben Viscomi <ruben.viscomi.dev@gmail.com>
2024-03-30 17:56:26 +0100

    feat(result): add callback to result instantiators

--- lib/result/instantiators.ts
+++ lib/result/instantiators.ts
@@ -8,20 +8,44 @@ import {
     RESULT_OK,
 } from "./constants";
 
-function result<T, E>(value: Ok<T> | Err<E>): Result<T, E> {
+function _result<T, E>(value: Ok<T> | Err<E>): Result<T, E> {
     return new _Result(value);
 }
 
 export function ok<T, E>(value: T): Result<T, E> {
-    return result({
+    return _result({
         [OK_FIELD_VALUE]: value,
         [RESULT_FIELD_DISCRIMINATOR]: RESULT_OK,
     });
 }
 
 export function err<T, E>(value: E): Result<T, E> {
-    return result({
+    return _result({
         [ERR_FIELD_VALUE]: value,
         [RESULT_FIELD_DISCRIMINATOR]: RESULT_ERR,
     });
+}
+
+export function throwableToResult<T>(throwable: () => T): Result<T, unknown>;
+export function throwableToResult<T, E>(throwable: () => T, converter: (error: unknown) => E): Result<T, E>;
+export function throwableToResult<T, E = unknown>(throwable: () => T, converter?: (error: unknown) => E) {
+    try {
+        return ok(throwable())
+    } catch (error) {
+        return !!converter
+            ? err(converter(error))
+            : err(error);
+    }
+}
+
+export async function throwableAsyncToResult<T>(throwable: () => Promise<T>): Promise<Result<T, unknown>>
+export async function throwableAsyncToResult<T, E>(throwable: () => Promise<T>, converter: (error: unknown) => E): Promise<Result<T, E>>
+export async function throwableAsyncToResult<T, E = unknown>(throwable: () => Promise<T>, converter?: (error: unknown) => E) {
+    try {
+        return ok(await throwable());
+    } catch (error) {
+        return !!converter
+            ? err(converter(error))
+            : err(error);
+    }
 }
