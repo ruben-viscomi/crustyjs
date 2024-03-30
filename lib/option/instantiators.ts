2afa854ce658cf2d0816a4c6913ba199e15cf648
Ruben Viscomi <ruben.viscomi.dev@gmail.com>
2024-03-30 17:27:17 +0100

    feat(option): add option instatiator
    Automatically infer None or Some from the provided value.

--- lib/option/instantiators.ts
+++ lib/option/instantiators.ts
@@ -1,18 +1,25 @@
+import { _isNil } from "./guards";
 import { _Option } from "./implementation";
 import { None, Option, Some } from "./types";
 
-function option<T>(value: Some<T> | None): Option<T> {
+function _option<T>(value: Some<T> | None): Option<T> {
     return new _Option<T>(value);
 }
 
+export function option<T>(value: T | null | undefined): Option<T> {
+    return _isNil(value)
+        ? none()
+        : some(value);
+}
+
 export function none<T>(): Option<T> {
-    return option({
+    return _option({
         [OPTION_FIELD_DISCRIMINATOR]: OPTION_NONE,
     });
 }
 
 export function some<T>(value: T): Option<T> {
-    return option({
+    return _option({
         [SOME_FIELD_VALUE]: value,
         [OPTION_FIELD_DISCRIMINATOR]: OPTION_SOME,
     });
