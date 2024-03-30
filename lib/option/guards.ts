e3c3ef45b320db25bd3ced7047e8ec71b0f80582
Ruben Viscomi <ruben.viscomi.dev@gmail.com>
2024-03-30 17:25:51 +0100

    refactor(guards): improve type guards

--- lib/option/guards.ts
+++ lib/option/guards.ts
@@ -1,13 +1,14 @@
 import { None, Some } from "./types";
 
+export function _isNil(value: unknown): value is undefined | null {
+    return value === undefined || value === null;
+}
+
 export function _isSome<T>(value: unknown): value is Some<T> {
-    return typeof value === "object"
-        && !!value
-        && (value as Some<T>)[OPTION_FIELD_DISCRIMINATOR] === OPTION_SOME;
+    return (value as Some<T>)?.[OPTION_FIELD_DISCRIMINATOR] === OPTION_SOME
+        && !_isNil((value as Some<T>)?.[SOME_FIELD_VALUE]);
 }
 
 export function _isNone(value: unknown): value is None {
-    return typeof value === "object"
-        && !!value
-        && (value as None)[OPTION_FIELD_DISCRIMINATOR] === OPTION_NONE;
+    return (value as None)[OPTION_FIELD_DISCRIMINATOR] === OPTION_NONE;
 }
--- lib/result/guards.ts
+++ lib/result/guards.ts
@@ -2,13 +2,9 @@ import { RESULT_FIELD_DISCRIMINATOR, RESULT_ERR, RESULT_OK } from "./constants";
 import { Err, Ok } from "./types";
 
 export function _isOk<T>(value: unknown): value is Ok<T> {
-    return typeof value === "object"
-        && !!value
-        && (value as Ok<T>)[RESULT_FIELD_DISCRIMINATOR] === RESULT_OK;
+    return (value as Ok<T>)?.[RESULT_FIELD_DISCRIMINATOR] === RESULT_OK;
 }
 
 export function _isErr<T>(value: unknown): value is Err<T> {
-    return typeof value == "object"
-        && !!value
-        && (value as Err<T>)[RESULT_FIELD_DISCRIMINATOR] === RESULT_ERR;
+    return (value as Err<T>)?.[RESULT_FIELD_DISCRIMINATOR] === RESULT_ERR;
 }
