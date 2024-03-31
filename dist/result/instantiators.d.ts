01760bd50aadff1847e8f25be11784b10322edab
Ruben Viscomi <ruben.viscomi.dev@gmail.com>
2024-04-01 00:03:41 +0200

    chore(dist): generate files to distribute for version 1.0.0

--- dist/result/instantiators.d.ts
+++ dist/result/instantiators.d.ts
@@ -17,7 +17,7 @@ export declare function err<T, E>(value: E): Result<T, E>;
  * @param converter (optional) function to convert the thrown type to `E`, if not provided `E = unknown`.
  * @returns an instance of {@link Err} if `throwable` throws an error, {@link Ok} otherwise.
  */
-export declare function toResult<T>(throwable: () => T): Result<T, unknown>;
+export declare function toResult<T, E = unknown>(throwable: () => T): Result<T, E>;
 export declare function toResult<T, E>(throwable: () => T, converter: (error: unknown) => E): Result<T, E>;
 /**
  * The asynchronous version of {@link toResult}. Instantiates the {@link Result} from the provided async function.
@@ -25,5 +25,5 @@ export declare function toResult<T, E>(throwable: () => T, converter: (error: un
  * @param converter (optional) function to convert the thrown type to `E`, if not provided `E = unknown`.
  * @returns a {@link Promise} wrapping an instance of {@link Err} if `throwable` throws an error, {@link Ok} otherwise.
  */
-export declare function toResultAsync<T>(throwable: () => Promise<T>): Promise<Result<T, unknown>>;
+export declare function toResultAsync<T, E = unknown>(throwable: () => Promise<T>): Promise<Result<T, E>>;
 export declare function toResultAsync<T, E>(throwable: () => Promise<T>, converter: (error: unknown) => E): Promise<Result<T, E>>;
